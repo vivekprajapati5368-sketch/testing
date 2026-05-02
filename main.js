17afa51049622736dd07e711ae0b47eea52a548a
Vivek <vivekprajapati5368@gmail.com>
2026-05-02 13:23:17 +0530

    add a loop

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 console.log('Hello');
 setTimeout(() => {
     console.log('Hello after 2 seconds')
-}, 2000)
+}, 2000);
+
+for (let i = 0; i < 100; i++) {
+    setTimeout(() => {
+        console.log('Hello after 2 seconds')
+    }, 1000);
+}
